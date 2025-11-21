50177e8df1b0f9a6630b0114793bc74cfd4d0a52
vinaykodi77-bit <vinaykodi77@gmail.com>
2025-11-22 02:21:09 +0530

    Add files via upload

---
+++ upper or lower case using if else ladder 51.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int s;
+	scanf(" %c",&s);
+	if (s>='a'&&s<='z')||(s>='A'&&s<='Z'){
+		if (s>=a&&s<=z){
+			printf("lower case");
+		}
+		if (s>='A'&&s<='Z'){
+			printf("upper case");
+		}
+	}
+	return 0;
+}#include<stdio.h>
+int main(){
+	int s;
+	scanf(" %c",&s);
+	if (s>='a'&&s<='z')||(s>='A'&&s<='Z'){
+		if (s>=a&&s<=z){
+			printf("lower case");
+		}
+		if (s>='A'&&s<='Z'){
+			printf("upper case");
+		}
+	}
+	return 0;
+}
