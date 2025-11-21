a13cf471d1ac8d0f8ef6d4efe08d18bcf99fbde6
vinaykodi77-bit <vinaykodi77@gmail.com>
2025-11-22 02:10:40 +0530

    Add files via upload

---
+++ roots ofquadratic equation 22.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int a,b,c;
+	float r1,r2;
+	scanf("%d%d%d",&a,&b,&c);
+	float d=b*b-(4*a*c);
+	if (d==0){
+		printf("equal\n");
+		r1=r2=-b/(2*a);
+		printf("root1=%.2f\n",r1);
+		printf("root2=%.2f\n",r2);
+	}
+	else if (d>0){
+		printf("distanct\n");
+		r1=(-b+sqrt((d)/2*a));
+		r2=(-b-sqrt((d)/2*a));
+			printf("root1=%.2f\n",r1);
+		printf("root2=%.2f\n",r2);
+	}
+	else if (d<0){
+		printf("imaginary\n");
+		printf("root1=%.2f+i%.2f\n",-b/(2.0*a),sqrt((-d)/(2*a)));
+		printf("root1=%.2f-i%.2f\n",-b/(2.0*a),sqrt((-d)/(2*a)));
+	}
+	return 0;
+}
