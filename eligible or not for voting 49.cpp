d0bf34d7829eabe2d92328182e3f01e965579369
vinaykodi77-bit <vinaykodi77@gmail.com>
2025-11-22 02:20:24 +0530

    Add files via upload

---
+++ eligible or not for voting 49.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("enter your age");
+scanf("%d",&n);
+	 if (n>=18){
+	 	printf(" he is eligible");
+	 }
+else 
+	printf(" he is not eligible");
+	return 0;
+}
