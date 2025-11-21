bb7b5ee77e51b5a640ef5cb0256346858fe60796
vinaykodi77-bit <vinaykodi77@gmail.com>
2025-11-22 02:17:06 +0530

    Add files via upload

---
+++ positive number or negitive numbers 40.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=5;
+	printf("%d",a);
+	if(a>0)
+	{
+		printf(" number is positive",a);
+	}
+	else
+	{
+		printf(" number is negative",a);
+	}
+	return 0;
+}
+
